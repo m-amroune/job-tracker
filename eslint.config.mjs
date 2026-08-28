9934314ed2bf1d4c289f3496cfd6fd50ec8edbbd
Moustapha Amroune <amroune.moustapha@gmail.com>
2026-08-28 08:20:10 +0200

    feat: add Storybook for JobCard

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
@@ -5,16 +8,20 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-
   {
     rules: {
       "react-compiler/react-compiler": "off",
       "react-hooks/set-state-in-effect": "off",
     },
   },
-
-  // Override default ignores of eslint-config-next.
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "storybook-static/**",
+    "next-env.d.ts",
+  ]),
+  ...storybook.configs["flat/recommended"],
 ]);
 
 export default eslintConfig;
